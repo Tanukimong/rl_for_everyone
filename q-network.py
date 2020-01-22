07186d244306679d5c7376477eae54ce92e08f0e
Tanukimong <35714982+Tanukimong@users.noreply.github.com>
2020-01-22 22:16:20 +0900

    Initial Setup

--- q-network.py
+++ q-network.py
@@ -1,66 +1,82 @@
 import gym
 import numpy as np
-import tensorflow.compat.v1 as tf
+from gym.envs.registration import register
+import tensorflow as tf
 import matplotlib.pyplot as plt
-
-tf.disable_eager_execution()
-
-env = gym.make('FrozenLake-v0')
+import gc
+import time
 
 # Parameters
-total_states = 16
+register(
+    id="FrozenLake-v3",
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name':'4x4', 'is_slippery':True}
+)
+
+env = gym.make('FrozenLake-v3')
+
 input_size = env.observation_space.n
 output_size = env.action_space.n
-lr = 0.1
 
-def one_hot(x):
-    return np.identity(total_states)[x:x+1]
+print("Input size :{}, Output size :{}".format(input_size,output_size))
 
-X = tf.placeholder(shape=[1, input_size], dtype=tf.float32)
-W = tf.Variable(tf.random_uniform([input_size, output_size],0,0.01))
+lr = 0.01
 
-Qpred   = tf.matmul(X,W)
-Y       = tf.placeholder(shape=[1,output_size], dtype=tf.float32)
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(input_size),
+    tf.keras.layers.Dense(output_size)
+])
 
-loss = tf.reduce_sum(tf.square(Y-Qpred))
-
-train = tf.train.GradientDescentOptimizer(learning_rate=lr).minimize(loss)
+model.compile(optimizer=tf.keras.optimizers.Adam(lr), loss='mse')
 
 dis = .99
 num_ep = 2000
 
 rList = []
-init = tf.global_variables_initializer()
 
 # Training code
-with tf.Session() as sess:
-    sess.run(init)
+
+start_time = time.time()
+
+with tf.device("GPU:0"):
     for i in range(num_ep):
+
+        print("Ep.{} Starting...".format(i))
+
         s = env.reset()
         e = 1./((i/50)+10)
         rAll = 0
         done = False
-        local_loss = []
-
         while not done:
-            Qs = sess.run(Qpred, feed_dict={X:one_hot(s)})
+            X = tf.one_hot(s,input_size)
+            X = tf.expand_dims(X,0)
+            Qs = model.predict(X,verbose=0)
+
             if np.random.rand(1) < e:
                 a = env.action_space.sample()
             else:
-                a = np.argmax(Qs)
-
-            s1, reward, done, _ = env.step(a)
+                a = tf.math.argmax(Qs,1)
+            s1, reward, done, _ = env.step(int(a))
             if done:
                 Qs[0, a]=reward
             else:
-                Qs1 = sess.run(Qpred, feed_dict={X: one_hot(s1)})
-                Qs[0,a] = reward + dis*np.max(Qs1)
-
-            sess.run(train, feed_dict={X: one_hot(s), Y:Qs})
+                X1 = tf.one_hot(s1,input_size)
+                X1 = tf.expand_dims(X1,0)
+                Qs1 = model.predict(X1,verbose=0)
+                Qs[0,a] = reward + dis*tf.reduce_max(Qs1)
+            
+            X = tf.expand_dims(tf.one_hot(s,input_size),0)
+            model.fit(X,Qs,verbose=0)
 
             rAll += reward
             s = s1
         rList.append(rAll)
+        time_cs = time.time()-start_time
+        print("From now = {:d}min {:d}sec\n".format(int(time_cs//60), int(time_cs%60)))
+
+
+model.summary()
+
 plt.plot(rList)
 
 print("Percent of successful eps: ", str(sum(rList)/num_ep)+"%")
